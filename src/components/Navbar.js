8870469ece5775461f9ab32df56c84c4aa7cdadd
adarsh <adarsh.19b401007@abes.ac.in>
2023-12-11 14:14:21 +0530

    fixing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import {addMoviesToList,handleMovieSearch} from '../actions'
 class  Navbar extends React.Component {
